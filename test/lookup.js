b70bbfc79662b0646f14536f676b67140225a8b6
Jonah Werre <jonahwerre@gmail.com>
2017-12-15 11:20:48 -1000

    completed lookup tests

--- test/lookup.js
+++ test/lookup.js
@@ -4,13 +4,23 @@ const 	assert = require('assert'),
 	
 describe( "Lookup", () => {
 	
+	let language = null
+	
 	describe( "Definitions", () => {
 		
 		it("should list global definitions", (done) => {
-			bundle = ['CountryLanguages','Industries','SampleTypes']
+			bundle = [
+				'CountryLanguages',
+				// 'Industries',
+				// 'SampleTypes'
+				]
 			fulcrum.lookup.definitions.listGlobalDefinitions(bundle)
 				.then(function(data){
 					assert.notEqual(data, null);
+					language = data.AllCountryLanguages.find(function (lang){
+						return lang.Code === 'ENG-US';
+					});
+					assert.notEqual(language, null)
 					done()
 				}).catch(function(err){
 					assert.equal(err, null);
@@ -47,10 +57,14 @@ describe( "Lookup", () => {
 
 	describe("Question Library", () => {
 		
+		let question = null
+		
 		it("should list standard questions", (done) => {
-			fulcrum.lookup.questionLibrary.listStandardQuestions()
+			fulcrum.lookup.questionLibrary.listStandardQuestions(language.Id)
 				.then(function(data){
 					assert.notEqual(data, null);
+					question = data.Questions[Math.floor(Math.random()*data.Questions.length)];
+					assert.notEqual(question, null);
 					done()
 				}).catch(function(err){
 					assert.equal(err, null);
@@ -59,7 +73,7 @@ describe( "Lookup", () => {
 		});
 		
 		it("should list custom questions", (done) => {
-			fulcrum.lookup.questionLibrary.listCustomQuestions()
+			fulcrum.lookup.questionLibrary.listCustomQuestions(language.Id)
 				.then(function(data){
 					assert.notEqual(data, null);
 					done()
@@ -70,9 +84,10 @@ describe( "Lookup", () => {
 		});
 		
 		it("should show question text", (done) => {
-			fulcrum.lookup.questionLibrary.showQuestionText()
+			fulcrum.lookup.questionLibrary.showQuestionText(language.Id, question.QuestionID)
 				.then(function(data){
 					assert.notEqual(data, null);
+					assert.notEqual(data.Question, null);
 					done()
 				}).catch(function(err){
 					assert.equal(err, null);
@@ -81,9 +96,11 @@ describe( "Lookup", () => {
 		});
 		
 		it("should show question options", (done) => {
-			fulcrum.lookup.questionLibrary.showQuestionOptions()
+			fulcrum.lookup.questionLibrary.showQuestionOptions(language.Id, question.QuestionID)
 				.then(function(data){
 					assert.notEqual(data, null);
+					assert.notEqual(data.QuestionOptions, null);
+					
 					done()
 				}).catch(function(err){
 					assert.equal(err, null);
